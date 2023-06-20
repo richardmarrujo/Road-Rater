6a568e235e39e7cf7c956e222ef2ad1f772a9eac
Richard Marrujo <89800641+richardmarrujo@users.noreply.github.com>
2023-06-19 20:47:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,45 @@ document.getElementById("logoutButton").addEventListener("click", function () {
 });
 
 // Use Enter as a submit button
-document.getElementById("Submit").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        // Prevent form submission
-        event.preventDefault();
+document.getElementById("Submit").addEventListener("click", function (event) {
+    // Prevent form submission
+    event.preventDefault();
 
-        // Get the form field values
-        var dateValue = "Date: " + document.getElementById("dateField").value;
-        var rateValue = "Rate: " + document.getElementById("rateField").value;
-        var plateField = "Plate: " + document.getElementById("plateField").value;
-        var commentsValue = "Comments: " + document.getElementById("commentsField").value;
-        
-        // Create an array to store the values
-        var fieldData = [dateValue, rateValue, plateField, commentsValue];
+    // Get the form field values
+    var dateValue = "Date: " + document.getElementById("dateField").value;
+    var rateValue = "Rate: " + document.getElementById("rateField").value;
+    var plateField = "Plate: " + document.getElementById("plateField").value;
+    var commentsValue = "Comments: " + document.getElementById("commentsField").value;
 
+    // Create an array to store the values
+    var fieldData = [dateValue, rateValue, plateField, commentsValue];
 
-        // Log the array for demonstration
-        console.log(fieldData);
+    // Log the array for demonstration
+    console.log(fieldData);
 
-        // Unhide the submitted text
-        var resultDiv = document.getElementById("submittedText");
-        resultDiv.style.opacity = 1;
-        resultDiv.style.height = "auto";
+    // Unhide the submitted text
+    var resultDiv = document.getElementById("submittedText");
+    resultDiv.style.opacity = 1;
+    resultDiv.style.height = "auto";
 
-        // Show the most recent array of form field values
-        var arrayDataDiv = document.getElementById("arrayData");
-        arrayDataDiv.textContent = "Most Recent Submission:\n" + fieldData.join("\n");
+    // Show the most recent array of form field values
+    var arrayDataDiv = document.getElementById("arrayData");
+    arrayDataDiv.textContent = "Most Recent Submission:\n" + fieldData.join("\n");
 
-        // Fade back out after a certain duration
-        setTimeout(function () {
-            submittedText.style.opacity = 0;
-            submittedText.style.height = 0;
-        }, 3000); // Change the duration (in milliseconds) as needed
-    }
+    // Fade back out after a certain duration
+    setTimeout(function () {
+        submittedText.style.opacity = 0;
+        submittedText.style.height = 0;
+    }, 3000); // Change the duration (in milliseconds) as needed
+
+    // Clear the form fields
+    document.getElementById("dateField").value = "";
+    document.getElementById("rateField").value = "";
+    document.getElementById("plateField").value = "";
+    document.getElementById("commentsField").value = "";
+
 });
+
 
 document.addEventListener("DOMContentLoaded", function () {
     var dropdown = document.querySelector(".dropdown");
