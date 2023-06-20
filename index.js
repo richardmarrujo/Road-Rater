075684e3be4eb0d06c0152bc6677847897016005
Richard Marrujo <89800641+richardmarrujo@users.noreply.github.com>
2023-06-19 20:13:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,30 +7,24 @@ document.getElementById("logoutButton").addEventListener("click", function () {
     window.location.href = "https://richardmarrujo.github.io/Road-Rater/login.html";
 });
 
-
 // Use Enter as a submit button
 document.getElementById("Submit").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         // Prevent form submission
-        event.preventDefault(); 
+        event.preventDefault();
 
         // Get the form field values
-        var dateValue = 'Date: ' + document.getElementById("dateField").value;
-        var rateValue = 'Rate: ' + document.getElementById("rateField").value;
-        var plateField = 'Plate: ' + document.getElementById("plateField").value;
-        var commentsValue = 'Comments: ' + document.getElementById("commentsField").value;
-
+        var dateValue = "Date: " + document.getElementById("dateField").value;
+        var rateValue = "Rate: " + document.getElementById("rateField").value;
+        var plateField = "Plate: " + document.getElementById("plateField").value;
+        var commentsValue = "Comments: " + document.getElementById("commentsField").value;
+        
         // Create an array to store the values
         var fieldData = [dateValue, rateValue, plateField, commentsValue];
 
+
         // Log the array for demonstration
         console.log(fieldData);
-
-        // Clear the form fields
-        document.getElementById("dateField").value = "";
-        document.getElementById("rateField").value = "";
-        document.getElementById("plateField").value = "";
-        document.getElementById("commentsField").value = "";
 
         // Unhide the submitted text
         var resultDiv = document.getElementById("submittedText");
@@ -40,33 +34,20 @@ document.getElementById("Submit").addEventListener("keydown", function (event) {
         // Show the most recent array of form field values
         var arrayDataDiv = document.getElementById("arrayData");
         arrayDataDiv.textContent = "Most Recent Submission:\n" + fieldData.join("\n");
-        arrayDataDiv.
 
         // Fade back out after a certain duration
         setTimeout(function () {
             submittedText.style.opacity = 0;
             submittedText.style.height = 0;
         }, 3000); // Change the duration (in milliseconds) as needed
-
     }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
     var dropdown = document.querySelector(".dropdown");
-    var arrow = dropdown.querySelector(".arrow");
+    var arrowSelect = dropdown.querySelector(".arrowSelect");
 
-    arrow.addEventListener("click", function () {
+    arrowSelect.addEventListener("click", function () {
         dropdown.classList.toggle("active");
     });
 });
-
-
-
-
-
-
-
-
-
-
-
